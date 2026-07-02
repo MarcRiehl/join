9c2e4be761c7041ba2a1963d93e205e3976659f5
Zohreh Habibi <habibi.z@outlook.de>
2026-07-02 12:27:03 +0200

    chore: remove unnecessary files

--- src/app/layout/test-layout/test-layout.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-test-layout',
-  imports: [],
-  templateUrl: './test-layout.html',
-  styleUrl: './test-layout.scss',
-})
-export class TestLayout {}
