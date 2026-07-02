6951132f7ef6c8e58bb239777b1aef4f97a6b6b9
MarcRiehl <210008956+MarcRiehl@users.noreply.github.com>
2026-07-02 11:55:11 +0200

    install: supabase

--- src/app/services/test-service.ts
+++ src/app/services/test-service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
+import { createClient } from '@supabase/supabase-js';
+import { environment } from '../../environments/environment'
+
 
 @Injectable({
   providedIn: 'root',
 })
-export class TestService {}
+  export class TestService {
+    supabase = createClient(environment.apiUrl, environment.apiKey);
+  }
