fb2566ba6bcadb644a0a8e663e188fd5aa227719
Franziska Kalloch <145328043+FranziskaKalloch@users.noreply.github.com>
2026-07-02 11:53:21 +0200

    generate component for user bubble

---
+++ src/app/layout/contact-bubble/contact-bubble.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-bubble',
+  imports: [],
+  templateUrl: './contact-bubble.html',
+  styleUrl: './contact-bubble.scss',
+})
+export class ContactBubble {}
