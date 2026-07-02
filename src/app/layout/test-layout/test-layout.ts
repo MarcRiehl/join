d06eed91d640afdb5a2860c91ba6801660dd2f29
MarcRiehl <210008956+MarcRiehl@users.noreply.github.com>
2026-07-02 07:46:26 +0200

    install: test-data

---
+++ src/app/layout/test-layout/test-layout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-layout',
+  imports: [],
+  templateUrl: './test-layout.html',
+  styleUrl: './test-layout.scss',
+})
+export class TestLayout {}
