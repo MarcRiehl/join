985b79f8aacc39c339755b0f4da05a02f206e912
Franziska Kalloch <145328043+FranziskaKalloch@users.noreply.github.com>
2026-07-03 10:31:45 +0200

    add mixin flexbox & border-radius

--- src/app/layout/contact-bubble/contact-bubble.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-contact-bubble',
-  imports: [],
-  templateUrl: './contact-bubble.html',
-  styleUrl: './contact-bubble.scss',
-})
-export class ContactBubble {}
