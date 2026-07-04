070a45c3f1f76ddeb53f3734feb0ecc4b2646473
Franziska Kalloch <145328043+FranziskaKalloch@users.noreply.github.com>
2026-07-04 11:34:37 +0200

    connected Supabase with Contect Service

--- src/app/services/contacts/contact.ts
+++ src/app/services/contacts/contact.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
+import { createClient } from '@supabase/supabase-js';
+
+import { Contact as ContactInterface } from '../../interfaces/contacts/contact';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Contact {}
+export class Contact {
+  supabaseUrl = 'https://awizaddljgzacbpunfhx.supabase.co/rest/v1/';
+  supabaseKey = 'sb_publishable_fQtC-jov7Yp1ouBIZaWweg_Ev2AADXG';
+  supabase = createClient(this.supabaseUrl, this.supabaseKey);
+}
