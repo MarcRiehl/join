0cb3e0d81fa3b93a4aaedeb7c52ad596ca684143
Zohreh Habibi <habibi.z@outlook.de>
2026-07-05 21:45:58 +0200

    feat(navbar): implement responsive navigation

--- src/app/layout/navbar/navbar.ts
+++ src/app/layout/navbar/navbar.ts
@@ -1,9 +1,59 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface NavItem {
+  label: string;
+  route: string;
+  icon: string;
+  activeIcon: string;
+}
+interface FooterLink {
+  label: string;
+  route: string;
+}
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar {}
+export class Navbar {
+  protected readonly navItems: NavItem[] = [
+    {
+      label: 'Summary',
+      route: '/summary',
+      icon: 'assets/img/navigation/summary.svg',
+      activeIcon: 'assets/img/navigation/summary-hover.svg',
+    },
+    {
+      label: 'Add Task',
+      route: '/add-task',
+      icon: 'assets/img/navigation/add-task.svg',
+      activeIcon: 'assets/img/navigation/add-task-hover.svg',
+    },
+    {
+      label: 'Board',
+      route: '/board',
+      icon: 'assets/img/navigation/board.svg',
+      activeIcon: 'assets/img/navigation/board-hover.svg',
+    },
+    {
+      label: 'Contacts',
+      route: '/contacts',
+      icon: 'assets/img/navigation/contacts.svg',
+      activeIcon: 'assets/img/navigation/contacts-hover.svg',
+    },
+  ];
+
+  protected readonly footerLinks: FooterLink[] = [
+    {
+      label: 'Privacy Policy',
+      route: '/privacy-policy',
+    },
+    {
+      label: 'Legal Notice',
+      route: '/legal-notice',
+    },
+  ];
+}
