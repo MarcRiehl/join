c42bb581a5b11a1a7441402b9a38f945e05e4a2a
Franziska Kalloch <145328043+FranziskaKalloch@users.noreply.github.com>
2026-07-06 15:31:22 +0200

    delete comments

--- src/app/pages/contacts/contact-list/contact-list.ts
+++ src/app/pages/contacts/contact-list/contact-list.ts
@@ -9,17 +9,11 @@ import { Contact as ContactInterface } from '../../../interfaces/contacts/contac
   templateUrl: './contact-list.html',
   styleUrl: './contact-list.scss',
 })
-
-// Die ContactList bekommt die Kontakte vom Parent über ein Input.
 export class ContactList {
   contacts = input<ContactInterface[]>([]);
-  contactSelected = output<ContactInterface>(); // “Ich habe einen Ausgang (Output), über den ich später einen ContactInterface an meinen Parent schicken kann.”
+  contactSelected = output<ContactInterface>();
 
   selectedContact(contact: ContactInterface) {
     this.contactSelected.emit(contact);
   }
 }
-
-//  emit() bedeutet: “Schicke diesen Kontakt an den Parent.”
-// * selectContact() = Methode wird beim Klick ausgeführt
-// * contactSelected = Event wird an Parent gesendet
