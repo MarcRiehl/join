90e04f08d66fcabda7e18592a9b9fb576c3a1018
Franziska Kalloch <145328043+FranziskaKalloch@users.noreply.github.com>
2026-07-13 10:33:27 +0200

    generate type for TaskCategory and TaskStatus

--- src/app/interfaces/task/task.types.ts
+++ src/app/interfaces/task/task.types.ts
@@ -1,1 +1,5 @@
 export type TaskPriority = 'urgent' | 'medium' | 'low';
+
+export type TaskCategory = 'Technical Task' | 'User Story';
+
+export type TaskStatus = 'to-do' | 'in progress' | 'await feedback' | 'done';
