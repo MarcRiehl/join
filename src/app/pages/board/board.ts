1f02e0ea4965ee51a555a2cd042493eebc5e96a0
MarcRiehl <210008956+MarcRiehl@users.noreply.github.com>
2026-07-14 19:03:05 +0200

    feat: task-dialog

--- src/app/pages/board/board.ts
+++ src/app/pages/board/board.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Contact as ContactInterface } from '../../interfaces/contacts/contact';
+import { DialogService, DialogType } from '../../services/dialog/dialog.service';
+import { ContactService } from '../../services/contacts/contact.service';
+import { TaskDialog } from './task-dialog/task-dialog';
 
 @Component({
   selector: 'app-board',
-  imports: [],
+  imports: [TaskDialog],
   templateUrl: './board.html',
   styleUrl: './board.scss',
 })
-export class Board {}
+export class Board {
+
+  readonly contactService = inject(ContactService);
+  readonly dialogService = inject(DialogService);
+  selectedContact = this.contactService.selectedContact;
+  readonly DialogType = DialogType;
+  // this.contactService.selectedContact.set(null);
+  openDialog(): void {
+
+    this.dialogService.open(DialogType.AddTask);
+  }
+}
---
+++ src/app/pages/board/task-dialog/task-dialog.ts
@@ -0,0 +1,214 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  inject,
+  computed
+} from '@angular/core';
+import { from, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators, AsyncValidatorFn } from '@angular/forms';
+import { DialogService } from '../../../services/dialog/dialog.service';
+import { ToastService } from '../../../services/toast/toast-service';
+import { ContactService } from '../../../services/contacts/contact.service';
+import { noPastDateValidator, getTodayDateString } from '../../../utils/date.util/date.util';
+import { fullNameValidator, splitFullName } from '../../../utils/name.util/name.util';
+
+@Component({
+  selector: 'app-task-dialog',
+  imports: [ReactiveFormsModule],
+  templateUrl: './task-dialog.html',
+  styleUrl: './task-dialog.scss',
+})
+export class TaskDialog implements AfterViewInit, OnInit {
+  private readonly dialogService = inject(DialogService);
+   private readonly contactService = inject(ContactService);
+   private toastService = inject(ToastService);
+ 
+   selectedContact = this.contactService.selectedContact;
+ 
+   isEditMode = computed(() => this.selectedContact() !== null);
+ 
+   @ViewChild('dialog')
+   dialog!: ElementRef<HTMLDialogElement>;
+ 
+   private isClosing = false;
+ 
+   ngAfterViewInit(): void {
+     this.dialog.nativeElement.showModal();
+   }
+ 
+   ngOnInit(): void {
+     const contact = this.selectedContact();
+ 
+     if (!contact) {
+       return;
+     }
+ 
+     this.addTaskForm.patchValue({
+       name: `${contact.firstname} ${contact.lastname}`,
+       email: contact.email,
+       phone: contact.phone?.toString() ?? ''
+     });
+ 
+   }
+ 
+   closeDialog(): void {
+     this.startCloseAnimation();
+   }
+ 
+   //protected
+   private startCloseAnimation(): void {
+ 
+     if (this.isClosing) {
+       return;
+     }
+ 
+     this.isClosing = true;
+     this.dialog.nativeElement.classList.add('closing');
+   }
+ 
+   onCancel(event: Event): void {
+     event.preventDefault();
+     this.startCloseAnimation();
+   }
+ 
+   onDialogClick(event: MouseEvent): void {
+ 
+     const dialog = this.dialog.nativeElement;
+     const rect = dialog.getBoundingClientRect();
+ 
+     const clickedInside =
+       event.clientX >= rect.left &&
+       event.clientX <= rect.right &&
+       event.clientY >= rect.top &&
+       event.clientY <= rect.bottom;
+ 
+     if (!clickedInside) {
+       this.startCloseAnimation();
+     }
+ 
+   }
+ 
+   animationFinished(event: AnimationEvent): void {
+     if (event.target !== this.dialog.nativeElement) {
+       return;
+     }
+ 
+     if (
+       event.animationName !== 'dialogOut' &&
+       event.animationName !== 'dialogOutMobile'
+     ) {
+       return;
+     }
+ 
+     const dialog = this.dialog.nativeElement;
+     dialog.classList.remove('closing');
+     dialog.close();
+     this.isClosing = false;
+     this.dialogService.clear();
+   }
+ 
+   addTaskForm = new FormGroup({
+     name: new FormControl('', {
+       validators: [Validators.required, Validators.pattern(/^[A-Za-zÄÖÜäöüß\s'-]+$/), fullNameValidator()],
+       asyncValidators: [this.nameValidator()],
+       updateOn: 'blur'
+     }),
+     email: new FormControl('', {
+       validators: [Validators.required, Validators.email],
+       updateOn: 'blur'
+     }),
+     phone: new FormControl('', {
+       validators: [Validators.required, Validators.pattern(/^[0-9+\s()-]+$/)],
+       updateOn: 'blur'
+     })
+   });
+ 
+   get name() {
+     return this.addTaskForm.controls.name;
+   }
+ 
+   get email() {
+     return this.addTaskForm.controls.email;
+   }
+ 
+   get phone() {
+     return this.addTaskForm.controls.phone;
+   }
+ 
+   formMessage = '';
+   messageType: 'success' | 'error' | '' = '';
+ 
+   nameValidator(): AsyncValidatorFn {
+     return (control: AbstractControl) => {
+       const value = (control.value ?? '').trim();
+ 
+       if (!value || control.errors) {
+         return of(null);
+       }
+ 
+       return from(
+         this.contactService.contactExists(
+           value,
+           this.selectedContact()?.id
+         )
+       ).pipe(
+         map(exists => (exists ? { nameExists: true } : null)),
+         catchError(() => of(null))
+       );
+     };
+   }
+ 
+ 
+   async onSubmit(): Promise<void> {
+ 
+     if (this.addTaskForm.invalid) {
+       this.addTaskForm.markAllAsTouched();
+       return;
+     }
+ 
+     const { firstname, lastname } = splitFullName(this.name.value!);
+     const editMode = this.isEditMode();
+ 
+     let success = false;
+ 
+     if (this.isEditMode()) {
+ 
+       success = await this.contactService.updateContact({
+         id: this.selectedContact()!.id,
+         firstname,
+         lastname,
+         email: this.email.value!,
+         phone: this.phone.value!
+       });
+ 
+     } else {
+ 
+       success = await this.contactService.addContact({
+         firstname,
+         lastname,
+         email: this.email.value!,
+         phone: this.phone.value!
+       });
+ 
+     }
+ 
+     if (success) {
+       this.addTaskForm.reset();
+       this.closeDialog();
+ 
+       if (!editMode) {
+         this.toastService.success('Contact successfully created.');
+       }
+     }
+   }
+ 
+   onRemoveSelectedContact() {
+     this.contactService.deleteSelectedContact();
+     this.closeDialog();
+   }
+ 
+ }
