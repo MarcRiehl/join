fe8e2442218c68a6014f1280d6358e514dcb472c
Zohreh Habibi <habibi.z@outlook.de>
2026-07-14 15:59:24 +0200

    restructure and implement AssignedTo component

--- src/app/pages/add-task/assigned-to/assigned-to/assigned-to.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-assigned-to',
-  imports: [],
-  templateUrl: './assigned-to.html',
-  styleUrl: './assigned-to.scss',
-})
-export class AssignedTo {}
