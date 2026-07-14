61d65478a07da95e51d884eab2fdc93ec6ac7cbb
Franziska Kalloch <145328043+FranziskaKalloch@users.noreply.github.com>
2026-07-14 11:03:13 +0200

    feat(tasks): add realtime synchronization

--- src/app/services/tasks/task.service.ts
+++ src/app/services/tasks/task.service.ts
@@ -1,4 +1,5 @@
 import { inject, Injectable, OnInit, signal } from '@angular/core';
+import { RealtimeChannel } from '@supabase/supabase-js';
 
 import { Task } from '../../interfaces/task/task';
 import { SupabaseService } from '../supabase/supabase.service';
@@ -8,7 +9,7 @@ import { SupabaseService } from '../supabase/supabase.service';
 })
 export class TaskService {
   private supabase = inject(SupabaseService);
-
+  private taskChannel: RealtimeChannel | undefined;
   tasks = signal<Task[]>([]);
 
   async loadTasks(): Promise<Task[]> {
@@ -94,21 +95,24 @@ export class TaskService {
     await this.loadTasks();
   }
 
-  // ----------------------------
-  // TASKS
-  // ----------------------------
-  // Alle Tasks laden (check)
-  // Einen Task nach ID laden
-  // Neuen Task erstellen (check)
-  // Task bearbeiten (check)
-  // Task löschen (check)
-  // Taskstatus ändern
-  // (To Do -> Done)
-  // Kategorie ändern
-  // Priorität ändern
-  // Zugewiesene Kontakte ändern
-  // Subtasks aktualisieren
-  // Tasks filtern
-  // Tasks suchen
-  // Tasks sortieren
+  subscribeToTaskChanges() {
+    this.taskChannel = this.supabase.supabase
+      .channel('task-changes')
+      .on(
+        'postgres_changes',
+        { event: '*', schema: 'public', table: 'tasks' },
+
+        async () => {
+          await this.loadTasks();
+        },
+      )
+      .subscribe();
+  }
+
+  async unsubscribeFromTaskChanges(): Promise<void> {
+    if (this.taskChannel) {
+      await this.supabase.supabase.removeChannel(this.taskChannel);
+      this.taskChannel = undefined;
+    }
+  }
 }
