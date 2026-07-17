c42ec88e718de91d019ef73461c29482c455a6be
Franziska Kalloch <145328043+FranziskaKalloch@users.noreply.github.com>
2026-07-17 15:30:07 +0200

    feat(board): add task search filter in task-view

--- src/app/pages/board/task-view/task-view.ts
+++ src/app/pages/board/task-view/task-view.ts
@@ -1,4 +1,5 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+
 import { Task } from '../../../interfaces/task/task';
 import { TaskStatus } from '../../../interfaces/task/task.types';
 import { TaskService } from '../../../services/tasks/task.service';
@@ -14,9 +15,14 @@ import { TaskCardComponent } from './task-card/task-card';
 export class TaskView {
   @Input({ required: true }) title!: string;
   @Input({ required: true }) status!: TaskStatus;
+  @Input() searchTerm = '';
   private taskService = inject(TaskService);
 
   get tasks(): Task[] {
-    return this.taskService.getTasksByStatus(this.status);
+    const task = this.taskService.getTasksByStatus(this.status);
+
+    return task.filter((task) => {
+      return task.title.includes(this.searchTerm) || task.description.includes(this.searchTerm);
+    });
   }
 }
