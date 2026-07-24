6c96839c4a7ada3960c602041d768a3fc71af961
Franziska Kalloch <145328043+FranziskaKalloch@users.noreply.github.com>
2026-07-24 12:52:33 +0200

    Login Formgroup

--- src/app/pages/log-in/log-in.ts
+++ src/app/pages/log-in/log-in.ts
@@ -1,17 +1,23 @@
 import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-log-in',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './log-in.html',
   styleUrl: './log-in.scss',
 })
 export class LogIn {
   private authService = inject(AuthService);
   private router = inject(Router);
+
+  addLoginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+  });
 
   // for Log-In Button:
 
